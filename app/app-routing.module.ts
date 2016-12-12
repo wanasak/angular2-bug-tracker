86b23fc045a71d3f37007c565c78626cfaacfb94
Wanasak <u510610433@gmail.com>
2016-12-12 15:42:52 +0700

    Created the Bug Module's Routing

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { BugListComponent } from './bugs/bug-list/bug-list.component';
-
 @NgModule({
     imports: [ 
         RouterModule.forRoot([
-            { path: '', component: BugListComponent }
+            
         ])
     ],
     exports: [ RouterModule ]
--- app/bugs/bug.module.ts
+++ app/bugs/bug.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { BugRoutingModule } from './bug-routing.module';
 
 // Component
 import { BugListComponent } from './bug-list/bug-list.component';
 
 @NgModule({
-    imports: [ SharedModule ],
+    imports: [ 
+        SharedModule,
+        BugRoutingModule,
+    ],
     declarations: [ BugListComponent ],
     exports: [ ],
     providers: [ ]
