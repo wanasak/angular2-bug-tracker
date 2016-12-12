9a49b8e3dce1af03ba8801706a119bd44ff10854
Wanasak <u510610433@gmail.com>
2016-12-12 15:13:39 +0700

    Created the Bug List Component

---
+++ app/bugs/bug-list/bug-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'bug-list',
+    templateUrl: 'bug-list.component.html',
+    styleUrls: [ 'bug-list.component.css' ]
+})
+export class BugListComponent {
+
+}
--- app/bugs/bug.module.ts
+++ app/bugs/bug.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
+// Component
+import { BugListComponent } from './bug-list/bug-list.component';
+
 @NgModule({
     imports: [ SharedModule ],
-    declarations: [ ],
+    declarations: [ BugListComponent ],
     exports: [ ],
     providers: [ ]
 })
