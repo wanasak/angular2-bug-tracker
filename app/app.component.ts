c059d7aff9bdb890d75c4135eabe832c9a77416d
Wanasak <u510610433@gmail.com>
2016-12-12 19:41:49 +0700

    Created the Bug List Template

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `
         <navbar></navbar>
+        <div class="container">
         <router-outlet></router-outlet>
+        </div>
     `
 })
 export class AppComponent { }
