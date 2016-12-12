f188f4864c8c922047660439a9faf1b161a75322
Wanasak <u510610433@gmail.com>
2016-12-12 22:24:44 +0700

    Created the Bug Service

---
+++ app/bugs/service/bug.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+import { FirebaseConfigService } from '../../core/service/firebase-config.service';
+
+
+@Injectable()
+export class BugService {
+    constructor(
+        private firebaseService: FirebaseConfigService
+    ) {
+
+    }
+}
