50d94932fe993da09569ca412715e0d9fb223478
Wanasak <u510610433@gmail.com>
2016-12-20 00:11:11 +0700

    Create Reactive From Using FormBuilder

--- app/bugs/bug-detail/bug-detail.component.ts
+++ app/bugs/bug-detail/bug-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 import { forbiddenStringValidator } from '../../shared/validator/forbidden-string.validator';
 
@@ -14,17 +14,27 @@ export class BugDetailComponent implements OnInit {
     private moduleId = "bugModal";
     private bugForm: FormGroup;
 
+    constructor(
+        private formBuilder: FormBuilder
+    ) { }
+
     ngOnInit() {
         this.configureForm();
     }
 
     configureForm() {
-        this.bugForm = new FormGroup({
-            title: new FormControl(null, [Validators.required, forbiddenStringValidator(/smudger/i)]),
-            status: new FormControl(1, Validators.required),
-            severity: new FormControl(1, Validators.required),
-            description: new FormControl(null, Validators.required),
-        })
+        // this.bugForm = new FormGroup({
+        //     title: new FormControl(null, [Validators.required, forbiddenStringValidator(/smudger/i)]),
+        //     status: new FormControl(1, Validators.required),
+        //     severity: new FormControl(1, Validators.required),
+        //     description: new FormControl(null, Validators.required),
+        // })
+       this.bugForm = this.formBuilder.group({
+           title: [null, [Validators.required, forbiddenStringValidator(/smudger/i)]],
+           status: [1, Validators.required],
+           severity: [1, Validators.required],
+           description: [null, Validators.required]
+       });
     }
 
     submitForm() {
